fff354cb3af6b21434e78b6221827144df9d7d52
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-07 16:19:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array)
   lang_collection = ["ruby", "javascript", "python", "objective-c"]
+  i = 0
   upcase_lang = []
   while array[i].downcase
     upcase_lang << array[i].upcase
+    i += 1
   end
   upcase_lang
 end
