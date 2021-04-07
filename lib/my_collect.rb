cd05b44dff48ac7c2aefdfa55b01883705caf015
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-07 16:14:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
 lang_collection = ["ruby", "javascript", "python", "objective-c"]
 
 def my_collect(array)
+  upcase_lang = []
   lang_collection.join(" ")
   while string.downcase
-  
+    upcase_lang << string.upcase
+  end
+  string.split(", ")
 
 end
